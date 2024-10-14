4516ed0c23be11388d85758847bb6c02cf07d45b
khangcntt2002 <minhkhang220602@gmail.com>
2024-10-14 20:14:08 +0700

    khangcanchi

---
+++ src/spc/edu/Bai11_TinhCanChi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author SPC
+ */
+public class Bai11_TinhCanChi {
+ 
+public static void main(String[] args) {
+    String[] can = {"canh","tan","nham","quy","giap","at","binh","Dinh","Mau","Ky"};
+    String[] Chi = {"than","Dau","Tuat","Hoi","Ty","Suu","Dan","Meo","Thinh","Ti","Ngo","Mui"} ;
+    
+    System.out.println("Truong Trinh Tinh Can Chi");
+    Scanner SC = new Scanner(System.in);
+    System.out.print("Nhap Nam: ");
+    int nam,namcan,namchi;
+    
+    nam = SC.nextInt();
+    namcan = nam % 10;
+    namchi = nam % 12;
+    System.out.print(can[namcan] + " ");
+    System.out.println(Chi[namchi]);
+    
+            
+}
+  
+    
+}
